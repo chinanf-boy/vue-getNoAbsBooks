79194f35f534430660a59ad95eb46ac907f9122f
chinanf-boy <865501259@qq.com>
2018-05-14 15:26:57 +0800

    Fix process.env

--- src/http.js
+++ src/http.js
@@ -6,7 +6,7 @@ const axios = require('axios');
 
 axios.defaults.timeout = 5000;
 
-if(!process.env.BOOK_DEV){
+if(process.env.BOOK_DEV){
     axios.defaults.baseURL = "http://192.168.11.103:5000";
 }else{
     axios.defaults.baseURL = "https://getnoabsbooks.herokuapp.com";
