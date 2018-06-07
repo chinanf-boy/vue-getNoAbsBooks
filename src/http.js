d36bad85623bc45babe17c006ef98258eed82e8d
chinanf-boy <865501259@qq.com>
2018-06-07 09:30:48 +0800

    Fix pageButton

--- src/http.js
+++ src/http.js
@@ -5,10 +5,10 @@ const axios = require("axios");
 
 axios.defaults.timeout = 5000;
 
-if (process.env.BOOK_DEV) {
-  axios.defaults.baseURL = "http://192.168.11.103:5000";
-} else {
+if (!process.env.BOOK_DEV) {
   axios.defaults.baseURL = "https://getnoabsbooks.herokuapp.com";
+} else {
+  axios.defaults.baseURL = "http://192.168.11.103:5000";
 }
 
 // console.log(process.env.BookDev);
